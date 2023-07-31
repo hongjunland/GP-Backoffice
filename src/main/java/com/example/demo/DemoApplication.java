4e879cbed88808ba5fc379cba4790d283bf45451
taeyun1215 <taeyun1215@naver.com>
2023-07-31 09:20:37 +0900

    test111222

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	// dsdsds
+	// dsdsdsssssssssdddddddddd
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
